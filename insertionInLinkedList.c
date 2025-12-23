702821a7acf80a89c88767881af6a06267777583
karthikreddy944 <karthikreddy.cs24@bmsce.ac.in>
2025-12-23 19:20:34 +0530

    Update insertionInLinkedList.c

--- insertionInLinkedList.c
+++ insertionInLinkedList.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct Node {
     int data;
     struct Node* next;
 };
 
+struct Node* head = NULL;
 
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (newNode == NULL) {
-        printf("Memory allocation failed.\n");
+        printf("Memory allocation failed\n");
         exit(0);
     }
     newNode->data = data;
@@ -19,13 +19,12 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
-
-void display(struct Node* head) {
-    if (head == NULL) {
-        printf("List is empty.\n");
+void display() {
+    struct Node* temp = head;
+    if (temp == NULL) {
+        printf("List is empty\n");
         return;
     }
-    struct Node* temp = head;
     printf("Linked List: ");
     while (temp != NULL) {
         printf("%d -> ", temp->data);
@@ -34,101 +33,86 @@ void display(struct Node* head) {
     printf("NULL\n");
 }
 
-void insertAtBeginning(struct Node** head, int data) {
+void insertAtBeginning(int data) {
     struct Node* newNode = createNode(data);
-    newNode->next = *head;
-    *head = newNode;
+    newNode->next = head;
+    head = newNode;
 }
 
-
-void insertAtEnd(struct Node** head, int data) {
+void insertAtEnd(int data) {
     struct Node* newNode = createNode(data);
-    if (*head == NULL) {
-        *head = newNode;
+    if (head == NULL) {
+        head = newNode;
         return;
     }
-    struct Node* temp = *head;
+    struct Node* temp = head;
     while (temp->next != NULL) {
         temp = temp->next;
     }
     temp->next = newNode;
 }
 
-void insertAtPosition(struct Node** head, int data, int position) {
+void insertAtPosition(int data, int position) {
     struct Node* newNode = createNode(data);
-
-
     if (position == 1) {
-        newNode->next = *head;
-        *head = newNode;
+        newNode->next = head;
+        head = newNode;
         return;
     }
-
-    struct Node* temp = *head;
+    struct Node* temp = head;
     int i;
     for (i = 1; i < position - 1 && temp != NULL; i++) {
         temp = temp->next;
     }
-
-
     if (temp == NULL) {
-        printf("Invalid position! List has fewer elements.\n");
+        printf("Invalid position\n");
         free(newNode);
         return;
     }
-
     newNode->next = temp->next;
     temp->next = newNode;
 }
 
-
 int main() {
-    struct Node* head = NULL;
     int choice, data, position;
 
     while (1) {
-        printf("\n--- Singly Linked List Menu ---\n");
-        printf("1. Insert at Beginning\n");
-        printf("2. Insert at End\n");
-        printf("3. Insert at Any Position\n");
-        printf("4. Display List\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
+        printf("\n1.Insert at Beginning\n2.Insert at End\n3.Insert at Position\n4.Display\n5.Exit\n");
+        printf("Enter choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter data to insert at beginning: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
-                insertAtBeginning(&head, data);
+                insertAtBeginning(data);
                 break;
 
             case 2:
-                printf("Enter data to insert at end: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
-                insertAtEnd(&head, data);
+                insertAtEnd(data);
                 break;
 
             case 3:
-                printf("Enter data to insert: ");
+                printf("Enter data: ");
                 scanf("%d", &data);
                 printf("Enter position: ");
                 scanf("%d", &position);
-                insertAtPosition(&head, data, position);
+                insertAtPosition(data, position);
                 break;
 
             case 4:
-                display(head);
+                display();
                 break;
 
             case 5:
-                printf("Exiting program.\n");
                 exit(0);
 
             default:
-                printf("Invalid choice! Try again.\n");
+                printf("Invalid choice\n");
         }
     }
-
     return 0;
 }
+
